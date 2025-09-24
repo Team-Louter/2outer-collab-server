6b5076cf5591cb1a68ae201c8d3d115418d80e26
이도연 <dgsw0509@dgsw.hs.kr>
2025-09-24 16:07:59 +0900

    feat : userDomain

--- src/main/java/com/louter/collab/auth/domain/User.java
+++ src/main/java/com/louter/collab/auth/domain/User.java
@@ -1,5 +1,42 @@
 package com.louter.collab.auth.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String userEmail;
+
+    @Column(name = "password", nullable = false)
+    private String userPassword;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
 
 }
