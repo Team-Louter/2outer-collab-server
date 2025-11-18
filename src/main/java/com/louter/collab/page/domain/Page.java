849c5e12f0252238bfcc5d017349ac24039e9cb8
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-11-18 20:59:56 +0900

    feat : domain 추가

--- src/main/java/com/louter/collab/page/domain/Page.java
+++ src/main/java/com/louter/collab/page/domain/Page.java
@@ -1,4 +1,31 @@
 package com.louter.collab.page.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "pages")
+@Builder
 public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "page_id", nullable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
 }
--- src/main/java/com/louter/collab/page/domain/PageChange.java
+++ src/main/java/com/louter/collab/page/domain/PageChange.java
@@ -1,4 +1,28 @@
 package com.louter.collab.page.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "page_changes")
+@Builder
 public class PageChange {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "change_id", nullable = false)
+    private Long id;
+
+    @Column(name = "change_content", nullable = false)
+    private String changeContent;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
 }
