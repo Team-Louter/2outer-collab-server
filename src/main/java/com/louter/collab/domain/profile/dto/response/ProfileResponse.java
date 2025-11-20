c1f634dc9dec3547b4f3c766dbd309bdb486c1a6
이도연 <dgsw0509@dgsw.hs.kr>
2025-11-20 23:25:12 +0900

    feat : profile response

--- src/main/java/com/louter/collab/domain/profile/dto/response/ProfileResponse.java
+++ src/main/java/com/louter/collab/domain/profile/dto/response/ProfileResponse.java
@@ -1,12 +1,43 @@
 package com.louter.collab.domain.profile.dto.response;
 
+import com.louter.collab.domain.profile.entity.Profile;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
 @Builder
 public class ProfileResponse {
+    @NotNull
+    private Long userId;
 
+    @NotNull
+    private String userName;
+
+    @NotNull
+    private String profileImageUrl;
+
+    private List<String> projects;
+    private String bio;
+
+    @NotNull
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static ProfileResponse from(Profile profile, String userName, List<String> projects) {
+        return ProfileResponse.builder()
+                .userId(profile.getUserId())
+                .userName(userName)
+                .profileImageUrl(profile.getProfileImageUrl())
+                .projects(projects)
+                .bio(profile.getBio())
+                .createdAt(profile.getCreatedAt())
+                .updatedAt(profile.getUpdatedAt())
+                .build();
+    }
 }
--- src/main/java/com/louter/collab/domain/profile/entity/Profile.java
+++ src/main/java/com/louter/collab/domain/profile/entity/Profile.java
@@ -3,6 +3,7 @@ package com.louter.collab.domain.profile.entity;
 import com.louter.collab.domain.auth.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -26,6 +27,7 @@ public class Profile {
     private User user;
 
     @Column(name = "profile_image_url")
+    @ColumnDefault("http://api.teamcollab.site/api/files/download/02c775b8-2792-44f2-955c-9c08d0ec4d10_defaultProfileImage.png")
     private String profileImageUrl;
 
     @Column(name = "bio")
