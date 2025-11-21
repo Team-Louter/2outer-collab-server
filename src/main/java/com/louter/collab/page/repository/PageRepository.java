948c506644c2653bcd9e0319c789852c7da12491
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-11-21 11:16:08 +0900

    feat : pull 받기전에 쫄리니까

--- src/main/java/com/louter/collab/page/repository/PageRepository.java
+++ src/main/java/com/louter/collab/page/repository/PageRepository.java
@@ -4,4 +4,5 @@ import com.louter.collab.page.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PageRepository extends JpaRepository<Page, Long> {
+    Long id(Long id);
 }
