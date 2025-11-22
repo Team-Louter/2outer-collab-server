684a6db63c4fac18b55be6419fa24adc832d70c4
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-11-22 21:09:45 +0900

    fix : 에러 해결

--- src/main/java/com/louter/collab/domain/page/entity/PageCollaboratorId.java
+++ src/main/java/com/louter/collab/domain/page/entity/PageCollaboratorId.java
@@ -1,18 +1,27 @@
 package com.louter.collab.domain.page.entity;
 
-import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.Column;
 
-@Embeddable
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+import java.util.Objects;
+
 public class PageCollaboratorId {
+
+    @Column(name = "page_id")
     private Long pageId;
 
+    @Column(name = "user_id")
     private Long userId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PageCollaboratorId that)) return false;
+        return Objects.equals(pageId, that.pageId)
+                && Objects.equals(userId, that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pageId, userId);
+    }
 }
