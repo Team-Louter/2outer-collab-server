3aab84fb39e24f6f91667baaffcd44b15118f4cf
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-11-24 21:59:47 +0900

    feat : entity + repo+ dto 추가

--- src/main/java/com/louter/collab/domain/todo/dto/response/TodoResponse.java
+++ src/main/java/com/louter/collab/domain/todo/dto/response/TodoResponse.java
@@ -1,4 +1,27 @@
 package com.louter.collab.domain.todo.dto.response;
 
+import com.louter.collab.domain.todo.entity.Todo;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
 public class TodoResponse {
+    private Long todoId;
+    private String title;
+    private Boolean done;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static TodoResponse from(Todo todo) {
+        return TodoResponse.builder()
+                .todoId(todo.getTodoId())
+                .title(todo.getTitle())
+                .done(todo.getDone())
+                .createdAt(todo.getCreatedAt())
+                .updatedAt(todo.getUpdatedAt())
+                .build();
+    }
 }
--- src/main/java/com/louter/collab/domain/todo/entity/Todo.java
+++ src/main/java/com/louter/collab/domain/todo/entity/Todo.java
@@ -1,4 +1,36 @@
 package com.louter.collab.domain.todo.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "todos")
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "todo_id", nullable = false)
+    private Long todoId;
+
+    @Column(name = "title", length = 20, nullable = false)
+    private String title;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder.Default
+    private Boolean done = false;
 }
