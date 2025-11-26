6f99ea4c4e3a01304798364a916260962e53daca
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-11-26 23:42:56 +0900

    feat : service 추가

--- src/main/java/com/louter/collab/domain/notice/service/NoticeService.java
+++ src/main/java/com/louter/collab/domain/notice/service/NoticeService.java
@@ -1,4 +1,17 @@
 package com.louter.collab.domain.notice.service;
 
+import com.louter.collab.domain.notice.dto.request.NoticeCreateRequest;
+import com.louter.collab.domain.notice.dto.request.NoticeUpdateRequest;
+import com.louter.collab.domain.notice.dto.response.NoticeResponse;
+
+import java.util.List;
+
 public interface NoticeService {
+    NoticeCreateRequest createNotice(NoticeCreateRequest request);
+
+    NoticeResponse updateNotice(Long noticeId, NoticeUpdateRequest request);
+
+    void deleteNotice(Long noticeId);
+
+    List<NoticeResponse> getTodos();
 }
--- src/main/java/com/louter/collab/domain/notice/service/impl/NoticeServiceImpl.java
+++ src/main/java/com/louter/collab/domain/notice/service/impl/NoticeServiceImpl.java
@@ -1,4 +1,58 @@
 package com.louter.collab.domain.notice.service.impl;
 
-public class NoticeServiceImpl {
+import com.louter.collab.domain.notice.dto.request.NoticeCreateRequest;
+import com.louter.collab.domain.notice.dto.request.NoticeUpdateRequest;
+import com.louter.collab.domain.notice.dto.response.NoticeResponse;
+import com.louter.collab.domain.notice.entity.Notice;
+import com.louter.collab.domain.notice.repository.NoticeRepository;
+import com.louter.collab.domain.notice.service.NoticeService;
+import com.louter.collab.domain.todo.dto.response.TodoResponse;
+import com.louter.collab.global.common.exception.NoticeNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class NoticeServiceImpl implements NoticeService {
+
+    private final NoticeRepository noticeRepository;
+
+    @Override
+    @Transactional
+    public NoticeResponse createNotice(NoticeCreateRequest request) {
+        Notice notice = Notice.builder()
+                .title(request.getTitle())
+                .content(request.getContent())
+                .build();
+
+        return NoticeResponse.from(noticeRepository.save(notice));
+    }
+
+    @Override
+    @Transactional
+    public NoticeResponse updateNotice(Long noticeId, NoticeUpdateRequest request) {
+        Notice notice = noticeRepository.findById(noticeId)
+                .orElseThrow(() -> new NoticeNotFoundException("해당 공지를 찾을 수 없습니다 : " + noticeId));
+
+        return NoticeResponse.from(notice);
+    }
+
+    @Override
+    @Transactional
+    public void deleteNotice(Long noticeId) {
+        noticeRepository.deleteById(noticeId);
+    }
+
+    @Override
+    public List<NoticeResponse> getNotices() {
+        return noticeRepository.findAll().stream()
+                .map(NoticeResponse::from)
+                .collect(Collectors.toList());
+    }
+
 }
