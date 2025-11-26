ca3e2c1dbe0653516471a667d0ad9930b4fe67b4
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-11-26 20:37:26 +0900

    feat : dto-response 완

--- src/main/java/com/louter/collab/domain/notice/dto/response/NoticeResponse.java
+++ src/main/java/com/louter/collab/domain/notice/dto/response/NoticeResponse.java
@@ -1,4 +1,31 @@
 package com.louter.collab.domain.notice.dto.response;
 
+import com.louter.collab.domain.notice.entity.Notice;
+import com.louter.collab.domain.todo.dto.response.TodoResponse;
+import com.louter.collab.domain.todo.entity.Todo;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
 public class NoticeResponse {
+    private Long noticeId;
+    private String title;
+    private String content;
+    private Boolean check;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static NoticeResponse from(Notice notice) {
+        return NoticeResponse.builder()
+                .noticeId(notice.getNoticeId())
+                .title(notice.getTitle())
+                .content(notice.getContent())
+                .check(notice.getCheck())
+                .createdAt(notice.getCreatedAt())
+                .updatedAt(notice.getUpdatedAt())
+                .build();
+    }
 }
